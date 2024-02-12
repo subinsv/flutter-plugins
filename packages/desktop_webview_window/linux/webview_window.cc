601a52098cfd00af06abb02e7e18d8238d80d83d
Subin S V <subin.selvaraj@grootan.com>
2024-02-12 18:28:53 +0530

    Debug logs added

--- packages/desktop_webview_window/linux/webview_window.cc
+++ packages/desktop_webview_window/linux/webview_window.cc
@@ -34,32 +34,32 @@ namespace
     return GTK_WIDGET(web_view);
   }
 
-// void handle_script_message(WebKitUserContentManager* manager, WebKitJavascriptResult* message, gpointer data) {
-//   printf("handling handle_script_message");
+void handle_script_message(WebKitUserContentManager* manager, WebKitJavascriptResult* message, gpointer data) {
+  printf("handling handle_script_message");
 
-//   auto *window = static_cast<WebviewWindow *>(data);
-//   window->onJavaScriptMessage("test","test");
-// //  JSGlobalContextRef context = webkit_javascript_result_get_global_context(message);
-// //   JSValueRef value = webkit_javascript_result_get_value(message);
+  auto *window = static_cast<WebviewWindow *>(data);
+  window->onJavaScriptMessage("test1","testa");
+//  JSGlobalContextRef context = webkit_javascript_result_get_global_context(message);
+//   JSValueRef value = webkit_javascript_result_get_value(message);
 
 
-// //     auto *args = fl_value_new_map();
-// //     fl_value_set(args, fl_value_new_string("id"), fl_value_new_int(window_id_),"from js body");
-// //     fl_method_channel_invoke_method(
-// //         FL_METHOD_CHANNEL(method_channel_), "onJavaScriptMessage", args,
-// //         nullptr, nullptr, nullptr);
+//     auto *args = fl_value_new_map();
+//     fl_value_set(args, fl_value_new_string("id"), fl_value_new_int(window_id_),"from js body");
+//     fl_method_channel_invoke_method(
+//         FL_METHOD_CHANNEL(method_channel_), "onJavaScriptMessage", args,
+//         nullptr, nullptr, nullptr);
 
-// //     // Convert the JavaScript value to a string
-// //     // JSStringRef string_ref = JSValueToStringCopy(context, value, nullptr);
-// //     // char* message_str = JSStringCopyUTF8CString(string_ref);
-// //     // gsize length = JSStringGetLength(string_ref);
-// //     // JSStringRelease(string_ref);
-// //     printf("recevied message from javascr");
-// //     // Print the received message
-// //     // g_print("Received message from JavaScript: %.*s\n", (int)length, message_str);
+//     // Convert the JavaScript value to a string
+//     // JSStringRef string_ref = JSValueToStringCopy(context, value, nullptr);
+//     // char* message_str = JSStringCopyUTF8CString(string_ref);
+//     // gsize length = JSStringGetLength(string_ref);
+//     // JSStringRelease(string_ref);
+//     printf("recevied message from javascr");
+//     // Print the received message
+//     // g_print("Received message from JavaScript: %.*s\n", (int)length, message_str);
 
-// //     // g_free(message_str);
-// }
+//     // g_free(message_str);
+}
 
   void on_load_changed(WebKitWebView *web_view,
                        WebKitLoadEvent load_event,
@@ -198,7 +198,7 @@ void WebviewWindow::Close()
     auto *args = fl_value_new_map();
     fl_value_set(args, fl_value_new_string("id"), fl_value_new_int(window_id_));
     fl_value_set(args, fl_value_new_string("name"), fl_value_new_string(name));
-    fl_value_set(args, fl_value_new_string("body"), fl_value_new_string(""));
+    fl_value_set(args, fl_value_new_string("body"), fl_value_new_string(body));
     fl_method_channel_invoke_method(
         FL_METHOD_CHANNEL(method_channel_), "onJavaScriptMessage", args,
         nullptr, nullptr, nullptr);
@@ -248,18 +248,10 @@ void WebviewWindow::OnLoadChanged(WebKitLoadEvent load_event)
 void WebviewWindow::RegisterJavaScripInterface(const char *name)
 {
   printf("registering RegisterJavaScripInterface");
-  // auto *manager = webkit_web_view_get_user_content_manager(WEBKIT_WEB_VIEW(webview_));
-  //   g_signal_connect (manager, ("script-message-received::" + std::string(name)).c_str(),
-  //                 G_CALLBACK (handle_script_message), NULL);
-  //   webkit_user_content_manager_register_script_message_handler(manager, name);
-
-    auto *args = fl_value_new_map();
-    fl_value_set(args, fl_value_new_string("id"), fl_value_new_int(window_id_));
-    fl_value_set(args, fl_value_new_string("name"), fl_value_new_string(name));
-    fl_value_set(args, fl_value_new_string("body"), fl_value_new_string(""));
-    fl_method_channel_invoke_method(
-        FL_METHOD_CHANNEL(method_channel_), "onJavaScriptMessage", args,
-        nullptr, nullptr, nullptr);
+  auto *manager = webkit_web_view_get_user_content_manager(WEBKIT_WEB_VIEW(webview_));
+    g_signal_connect (manager, ("script-message-received::" + std::string(name)).c_str(),
+                  G_CALLBACK (handle_script_message), NULL);
+    webkit_user_content_manager_register_script_message_handler(manager, name);
 }
 
 void WebviewWindow::GoForward()
