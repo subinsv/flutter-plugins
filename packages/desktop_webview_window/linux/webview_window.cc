99207156ceb51d3b925609bfbca6a4d7dde766f7
Subin S V <subin.selvaraj@grootan.com>
2024-02-12 19:14:42 +0530

    Debug logs added

--- packages/desktop_webview_window/linux/webview_window.cc
+++ packages/desktop_webview_window/linux/webview_window.cc
@@ -38,12 +38,8 @@ void handle_script_message(WebKitUserContentManager* manager, WebKitJavascriptRe
   printf("handling handle_script_message");
 
   auto *window = static_cast<WebviewWindow *>(data);
-  g_critical("handle_script_message: %p", window);
-try {
   window->onJavaScriptMessage("test1","testa");
-}catch(const std::exception& e) {
-  g_critical("handle_script_message: %s", e.what());
-}
+
 //  JSGlobalContextRef context = webkit_javascript_result_get_global_context(message);
 //   JSValueRef value = webkit_javascript_result_get_value(message);
 
@@ -200,9 +196,9 @@ void WebviewWindow::Close()
 
  void WebviewWindow::onJavaScriptMessage(const char *name, const char *body)
   {
-    printf("onJavascriptMessage after handle");
-    // auto *args = fl_value_new_map();
-    // fl_value_set(args, fl_value_new_string("id"), fl_value_new_int(window_id_));
+    printf("onJavascriptMessage after handle %s", name);
+    auto *args = fl_value_new_map();
+    fl_value_set(args, fl_value_new_string("id"), fl_value_new_int(window_id_));
     // fl_value_set(args, fl_value_new_string("name"), fl_value_new_string(name));
     // fl_value_set(args, fl_value_new_string("body"), fl_value_new_string(body));
     // printf("invoke onJavascriptMessage to flutter");
